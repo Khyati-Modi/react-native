3e7bfcea999e047a77b1e5fab78f632bd5766315
Khyati-Modi <51826286+Khyati-Modi@users.noreply.github.com>
2020-01-26 19:43:36 +0530

    [wip] session 4 hw

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginComponent from './LoginComponent'
+
+export default function App() {
+  return (<LoginComponent></LoginComponent>)
+}
+
+
