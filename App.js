c2cb6d4a079354faadbb0fdb11d6e39317a83835
Khyati-modi <khyati.modi.sa@gmail.com>
2020-01-28 11:23:01 +0530

    formatting files

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginComponent from './LoginComponent'
+// import {StyleSheet, Text, View} from 'react-native';
+import LoginComponent from './LoginComponent';
 
 export default function App() {
-  return (<LoginComponent></LoginComponent>)
+  return <LoginComponent />;
 }
-
-
